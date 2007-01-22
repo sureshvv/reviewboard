18bb8fb33c90df01e115a24aeb5788db55e22a93
trowbrds <trowbrds@5efc13c4-1f27-0410-8691-ff2d1f55687e>
2007-01-22 09:25:52 +0000

    replace reviews.models.Person/Group with contrib.auth.User/Group

--- branches/user-rework/reviewboard/reviews/forms.py
+++ branches/user-rework/reviewboard/reviews/forms.py
@@ -1,7 +1,6 @@
 from django import newforms as forms
-from django.core import validators
-from django.core.validators import ValidationError
-from reviewboard.reviews.models import Group, Person, ReviewRequest
+from django.contrib.auth.models import User, Group
+from reviewboard.reviews.models import ReviewRequest
 
 class NewReviewRequestForm(forms.Form):
     summary = forms.CharField(max_length=300)
@@ -12,39 +11,35 @@ class NewReviewRequestForm(forms.Form):
     target_groups = forms.CharField()
     target_people = forms.CharField()
 
+    def create_from_list(data, constructor, error):
+        """Helper function to combine the common bits of clean_target_people
+           and clean_target_groups"""
+        result = []
+        names = [name.strip() for name in data.split(',')]
+        for name in names:
+            try:
+                result.append(constructor(name))
+            except:
+                raise forms.ValidationError(error % name)
+        return set(result)
+
+    def clean_target_people(self):
+        return create_from_list(self.clean_data['target_people'],
+                                lambda x: User.objects.get(username=x),
+                                'Reviewer "%s" does not exist')
+
+    def clean_target_groups(self):
+        return create_from_list(self.clean_data['target_groups'],
+                                lambda x: User.objects.get(name=x),
+                                'Group "%s" does not exist')
+
+    def clean(self):
+        if not self.clean_data['target_people'] and \
+           not self.clean_data['target_groups']:
+            raise forms.ValidationError(
+                'You must specify at least one reviewer or group')
+
     def create(self):
-        target_groups = self.clean_data['target_groups']
-        target_people = self.clean_data['target_people']
-        del(self.clean_data['target_groups'])
-        del(self.clean_data['target_people'])
-
         review_request = ReviewRequest(**self.clean_data)
-        review_request.save()
-
-        if target_groups and target_groups.strip().lower():
-            for group_name in target_groups.split(','):
-                group_name = group_name.strip().lower()
-                group, group_is_new = \
-                    Group.objects.get_or_create(name=group_name)
-
-                if group_is_new:
-                    group.save()
-
-                print 'group = %s, new = %s, id = %s' % \
-                    (group_name, group_is_new, group.id)
-                review_request.target_groups.add(group)
-
-
-        if target_people and target_people.strip().lower():
-            for person_name in target_people.split(','):
-                person_name = person_name.strip().lower()
-                person, person_is_new = \
-                    Person.objects.get_or_create(username=person_name)
-
-                if person_is_new:
-                    person.save()
-
-                review_request.target_people.add(person)
-
         review_request.save()
         return review_request
